730ad89c04d41f8caeecfac8ca187d3d45465538
Uwe Dauernheim <uwe@dauernheim.net>
2013-05-30 17:37:11 +0200

    Refactor code
    * Add documentation, referencing RFCs
    * Parametrize time stepper
    * Add `accept?` fun
    * Add `secret-key` fun
    * Add `base32-encode` fun
    * Add `base32-encode-data` fun?` fun

--- src/cljotp/core.clj
+++ src/cljotp/core.clj
@@ -1,31 +1,54 @@
 (ns cljotp.core
-  (:import (java.util Date)
-           (java.nio ByteBuffer))
-  (:require (cljotp (crypto :as crypto))))
+  (:import java.util.Date
+           java.nio.ByteBuffer)
+  (:require [cljotp.crypto :as crypto]))
 
-(declare get-timestamp)
 
-(defn get-hotp
-  "Get a one time token based on a secret and a
-  interval number."
-  [secret interval]
-  (let [secret (crypto/decode-base32 secret)
-        data (.array (.putLong (ByteBuffer/allocate 8) interval))
-        digest (crypto/sha_mac secret data)
+(declare time-steps)
+
+(defn hotp
+  "Return a one time token based on a secret and a interval number, as in
+   \"HOTP: An HMAC-Based One-Time Password Algorithm\" -
+   https://tools.ietf.org/html/rfc4226"
+  [secret counter]
+  (let [data (.array (.putLong (ByteBuffer/allocate 8) counter))
+        digest (crypto/hmac-sha1 secret data)
         offset (bit-and (digest 19) 0xf)
         code (bit-or (bit-shift-left (bit-and (digest offset) 0x7f) 24)
                      (bit-shift-left (bit-and (digest (+ offset 1)) 0xff) 16)
                      (bit-shift-left (bit-and (digest (+ offset 2)) 0xff) 8)
                      (bit-and (digest (+ offset 3)) 0xff))]
       (rem code 1000000)))
-  
-(defn get-totp
-  "Get a one time token based on the time."
-  [secret]
-  (let [interval (int (/ (get-timestamp (.getTime (Date.))) 30))]
-    (get-hotp secret interval)))
 
-(defn get-timestamp
-  "Convert milliseconds since epoch to a Unix Timestamp"
+(defn totp
+  "Return a one time token based on the time, as in \"TOTP: Time-Based
+  One-Time Password Algorithm\" - http://tools.ietf.org/html/rfc6238"
+  [secret]
+  ;; "We RECOMMEND a default time-step size of 30 seconds. This default value
+  ;; of 30 seconds is selected as a balance between security and usability."
+  (hotp secret (time-steps 30)))
+
+(defn accept?
+  "Validate an one-time password from a prover against a secret from a verifier."
+  ;; Because of possible clock drifts between a client and a validation server,
+  ;; we RECOMMEND that the validator be set with a specific limit to the number
+  ;; of time steps a prover can be "out of synch" before being rejected.
+  ([otp secret] (= otp (totp secret)))
+  ([otp secret counter] (= otp (hotp secret counter))))
+
+(defn secret-key
+  "Generate a random secret key."
+  []
+  ;; "Keys SHOULD be of the length of the HMAC output to facilitate
+  ;; interoperability."
+  (crypto/random-bytes 10))
+
+(defn- timestamp
+  "Convert milliseconds since epoch to a Unix Timestamp."
   [milliseconds]
   (int (/ milliseconds 1000)))
+
+(defn- time-steps
+  "Calculate the time steps from now (UTC) since epoch given a time step."
+  [time-step]
+  (int (/ (timestamp (.getTime (Date.))) time-step)))
--- src/cljotp/crypto.clj
+++ src/cljotp/crypto.clj
@@ -1,19 +1,40 @@
 (ns cljotp.crypto
-  (:import (org.apache.commons.codec.binary Base32)
-           (javax.crypto.spec SecretKeySpec)
-           (javax.crypto Mac)))
+  (:import org.apache.commons.codec.binary.Base32
+           java.security.SecureRandom
+           javax.crypto.spec.SecretKeySpec
+           javax.crypto.Mac))
 
-(defn sha_mac
-  "Return the SHA1 MAC digest for a given key and data"
+
+(defn hmac-sha1
+  "Return the HMAC-SHA-1 digest for a given secret and data."
   [secret, data]
   (let [spec (SecretKeySpec. secret "HmacSHA1")
         sha-mac (let [mac (Mac/getInstance "HmacSHA1")]
-                  (.init mac spec)
+                  (.init mac spec) ; mutable op!
                   mac)
-        hash (into [] (.doFinal sha-mac data))]
+        hash (into [] (.doFinal sha-mac data))] ; mutable op!
     hash))
 
-(defn decode-base32
+(defn random-bytes
+  "Generate a random byte array."
+  [size]
+  (let [bytes (byte-array size)]
+    ;; Native vs SHA1 PRNG?
+    ;; (.nextBytes (SecureRandom.) bytes) ; mutable op!
+    (.nextBytes (SecureRandom/getInstance "SHA1PRNG") bytes) ; mutable op!
+    bytes))
+
+(defn base32-encode-data
+  "Encode a binary to base32 string."
+  [data]
+  (.encodeToString (Base32.) data))
+
+(defn base32-encode
+  "Encode a string to base32 string."
+  [string]
+  (base32-encode-data (.getBytes string)))
+
+(defn base32-decode
   "Decode a base32 string to binary."
   [string]
   (.decode (Base32.) string))
