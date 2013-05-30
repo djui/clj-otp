c66d34d4963202ba663870d9022ef58adc6a6494
Uwe Dauernheim <uwe@dauernheim.net>
2013-05-30 17:37:38 +0200

    Update to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Create HOTP and TOTP"
   :url "http://github.com/omarkj/cljotp"
   :licence "See the LICENCE file"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [commons-codec "1.6"]])
