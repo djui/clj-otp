179078b941c3347e56fa5ee98531f5c2fb640315
ttasterisco <ttasterisco@gmail.com>
2014-04-23 21:01:22 -0700

    for this project to be usable as a library, the folder structure and package structure must match I decided for the former, since the project name is clj-otp

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-otp "0.1.1"
+(defproject clj-otp "0.1.2"
   :description "Create HOTP and TOTP"
   :url "http://github.com/djui/clj-otp"
   :licence "See the LICENCE file"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [commons-codec "1.6"]
                  [ring/ring-codec "1.0.0"]])
